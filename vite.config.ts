2f146aa7b2252131d79b4ce9563d072e69eb5862
cnerlab <cnerlab@gmail.com>
2025-06-01 10:40:46 +0530

    remove server route

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,26 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./app"),
+//     },
+//   },
+//   base: "/cnerlabs/",
+//   server: {
+//     open: true,
+//     port: 3000,
+//   },
+// });
+
 export default defineConfig({
+  base: "/cnerlabs/", // Important for GitHub Pages
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./app"),
     },
-  },
-  base: "/cnerlabs/",
-  server: {
-    open: true,
-    port: 3000,
   },
 });
